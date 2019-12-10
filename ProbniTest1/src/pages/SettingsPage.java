602548b2938ef450862c886bf18963a0fa56879c
Marko24-bit <markjevtic889@gmail.com>
2019-12-10 21:04:50 +0100

    test

---
+++ ProbniTest1/src/pages/SettingsPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SettingsPage extends BasicPage{
+
+	public SettingsPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
